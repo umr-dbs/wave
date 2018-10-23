ac97011acee559a70cb64248be6a612835f66d5e
Julian <jmaerte@mathematik.uni-marburg.de>
2018-10-23 23:59:34 +0200

    Selected time correlates with the project service time now for better visualization of the raster at the given time span. Make geo bon ebv layers accessible from drop down. TODO: make layer invisible at first.

--- src/app/components/ebv-selection/ebv-selection.component.ts
+++ src/app/components/ebv-selection/ebv-selection.component.ts
@@ -1,14 +1,27 @@
-
 import {Component, OnInit, ChangeDetectionStrategy, AfterViewInit, ViewChild} from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 import {VectorLayer, RasterLayer, Layer} from '../../layers/layer.model';
 import {ProjectService} from '../../project/project.service';
-import {ComplexVectorSymbology} from "../../layers/symbology/symbology.model";
+import {
+    ComplexVectorSymbology, RasterSymbology,
+    MappingColorizerRasterSymbology
+} from "../../layers/symbology/symbology.model";
 import {Operator} from "../../operators/operator.model";
 import {ResultTypes} from "../../operators/result-type.model";
 import {Plot} from "../../plots/plot.model";
 import {RScriptType} from "../../operators/types/r-script-type.model";
 import {TimeSelectionComponent} from "../time-selection/time-selection.component";
+import {RasterizePolygonType} from '../../operators/types/rasterize-polygon-type.model';
+import {ExpressionType} from '../../operators/types/expression-type.model';
+import {UserService} from '../../users/user.service';
+import {of as observableOf, Observable, BehaviorSubject} from 'rxjs';
+import {MappingSource, MappingSourceRasterLayer} from '../../operators/dialogs/data-repository/mapping-source.model';
+import {DataSource} from '@angular/cdk/collections';
+import {Unit} from '../../operators/unit.model';
+import {GdalSourceType} from '../../operators/types/gdal-source-type.model';
+import {SourceDatasetComponent} from '../../operators/dialogs/data-repository/raster/source-dataset.component';
+import {Projection, Projections} from '../../operators/projection.model';
+import {DataType, DataTypes} from '../../operators/datatype.model';
 
 
 @Component({
@@ -19,38 +32,60 @@ import {TimeSelectionComponent} from "../time-selection/time-selection.component
 })
 export class EBVComponent implements OnInit, AfterViewInit {
 
-    form: FormGroup;
     countryLayer: VectorLayer<ComplexVectorSymbology>;
-    ebvLayer: Layer;
+    ebvLayer: RasterLayer<RasterSymbology>;
 
-    ebv = [];
+    sources: Observable<Array<MappingSource>>;
+    ebv: Observable<Array<MappingSourceRasterLayer>>;
     @ViewChild('time') time: TimeSelectionComponent;
 
     constructor(private formBuilder: FormBuilder,
-                private projectService: ProjectService) {
-        this.form = this.formBuilder.group({
-            loginAuthority: ['gfbio', Validators.required],
-            password: ['', Validators.required],
-            staySignedIn: [true, Validators.required],
-        });
+                private projectService: ProjectService,
+                private userService: UserService) {
+        this.sources = this.userService.getRasterSourcesStream();
     }
 
     ngOnInit() {
+        this.sources.subscribe(sources => {
+            for(let i in sources) {
+                let s = sources[i];
+                if(s.name == 'GEO BON') {
+                    this.ebv = observableOf(s.rasterLayer);
+                }
+            }
+        })
     }
 
     ngAfterViewInit() {
-        // do this once for observables
-        setTimeout(() => this.form.updateValueAndValidity(), 0);
     }
 
-    setCountryLayer(layer: VectorLayer) {
+    setCountryLayer(layer: VectorLayer<ComplexVectorSymbology>, operator: Operator) {
         if (this.countryLayer !== null) {
             this.projectService.removeLayer(this.countryLayer);
         }
         this.projectService.addLayer(this.countryLayer = layer);
     }
 
-    setEBVLayer(layer: RasterLayer) {
+    setEBVLayer(channel: MappingSourceRasterLayer) {
+        const unit: Unit = channel.unit;
+        const mappingTransformation = channel.transform;
+        const doTransform = channel.hasTransform;
+
+        let operator = this.createGdalSourceOperator(channel, doTransform);
+
+        let colorizerConfig = channel.colorizer;
+        if (doTransform) {
+            colorizerConfig = SourceDatasetComponent.createAndTransformColorizer(colorizerConfig, mappingTransformation);
+        }
+
+        const layer = new RasterLayer({
+            name: channel.name,
+            operator: operator,
+            symbology: new MappingColorizerRasterSymbology({
+                unit: (doTransform) ? channel.transform.unit : unit,
+                colorizer: colorizerConfig,
+            }),
+        });
         if (this.ebvLayer !== null) {
             this.projectService.removeLayer(this.ebvLayer);
         }
@@ -60,6 +95,8 @@ export class EBVComponent implements OnInit, AfterViewInit {
     reload() {
         this.projectService.clearPlots();
         const countryOperator: Operator = this.countryLayer.operator;
+
+        const clippedLayer = this.addClip(countryOperator, this.ebvLayer);
 
         const operator_country: Operator = new Operator({
             operatorType: new RScriptType({
@@ -100,13 +137,13 @@ p = (
         scale_x_continuous(breaks = dates)
 )
 print(p)`,
-                resultType: countryOperator.resultType,
+                resultType: clippedLayer.operator.resultType,
             }),
             resultType: ResultTypes.PLOT,
-            projection: countryOperator.projection,
+            projection: clippedLayer.operator.projection,
             pointSources: undefined,
             lineSources: undefined,
-            polygonSources: [countryOperator],
+            polygonSources: [clippedLayer.operator],
         });
         const plot_country = new Plot({
             name: 'local plot',
@@ -114,5 +151,111 @@ print(p)`,
         });
 
         this.projectService.addPlot(plot_country);
+    }
+
+    addClip(polygonOperator: Operator, rasterLayer: RasterLayer<RasterSymbology>): RasterLayer<RasterSymbology> {
+        const rasterOperator: Operator = rasterLayer.operator;
+
+        const name: string = 'clip';
+
+        const rasterizePolygonOperator = new Operator({
+            operatorType: new RasterizePolygonType({}),
+            resultType: ResultTypes.RASTER,
+            projection: rasterOperator.projection,
+            attributes: rasterOperator.attributes,
+            dataTypes: rasterOperator.dataTypes,
+            units: rasterOperator.units,
+            polygonSources: [polygonOperator],
+        });
+
+        const expressionOperator = new Operator({
+            operatorType: new ExpressionType({
+                expression: `B > 0 ? A : NAN`,
+                datatype: rasterOperator.getDataType('value'),
+                unit: rasterOperator.getUnit('value'),
+            }),
+            resultType: ResultTypes.RASTER,
+            projection: rasterOperator.projection,
+            attributes: rasterOperator.attributes,
+            dataTypes: rasterOperator.dataTypes,
+            units: rasterOperator.units,
+            rasterSources: [rasterOperator, rasterizePolygonOperator],
+        });
+
+        const layer = new RasterLayer({
+            name: name,
+            operator: expressionOperator,
+            symbology: rasterLayer.symbology,
+        });
+
+        this.projectService.addLayer(layer);
+        return layer;
+    }
+
+
+    createGdalSourceOperator(channel: MappingSourceRasterLayer, doTransform: boolean): Operator {
+        const sourceDataType = channel.datatype;
+        const sourceUnit: Unit = channel.unit;
+        let sourceProjection: Projection;
+        if (channel.coords.crs) {
+            sourceProjection = Projections.fromCode(channel.coords.crs);
+        } else {
+            throw new Error('No projection or EPSG code defined in [GEO BON]. channel.id: ' + channel.id);
+        }
+
+        const operatorType = new GdalSourceType({
+            channel: channel.id,
+            sourcename: 'GEO BON',
+            transform: doTransform, // TODO: user selectable transform?
+        });
+
+        const sourceOperator = new Operator({
+            operatorType: operatorType,
+            resultType: ResultTypes.RASTER,
+            projection: sourceProjection,
+            attributes: ['value'],
+            dataTypes: new Map<string, DataType>().set('value', DataTypes.fromCode(sourceDataType)),
+            units: new Map<string, Unit>().set('value', sourceUnit),
+        });
+
+        // console.log('doTransform', doTransform, 'unit', sourceUnit, 'expression', 'A', 'datatype', sourceDataType, 'channel', channel);
+        if (doTransform && channel.hasTransform) {
+            const transformUnit = channel.transform.unit;
+            const transformDatatype = DataTypes.fromCode(channel.transform.datatype);
+
+            const transformOperatorType = new ExpressionType({
+                unit: transformUnit,
+                expression: '(A -' + channel.transform.offset.toString() + ') *' + channel.transform.scale.toString(),
+                datatype: transformDatatype,
+            });
+
+            const transformOperator = new Operator({
+                operatorType: transformOperatorType,
+                resultType: ResultTypes.RASTER,
+                projection: sourceProjection,
+                attributes: ['value'],
+                dataTypes: new Map<string, DataType>().set('value', transformDatatype),
+                units: new Map<string, Unit>().set('value', transformUnit),
+                rasterSources: [sourceOperator],
+            });
+            return transformOperator;
+        }
+
+        return sourceOperator;
+    }
+}
+class ChannelDataSource extends DataSource<MappingSourceRasterLayer> {
+    private channels: Array<MappingSourceRasterLayer>;
+
+    constructor(channels: Array<MappingSourceRasterLayer>) {
+        super();
+        this.channels = channels;
+    }
+
+    connect(): Observable<Array<MappingSourceRasterLayer>> {
+        return observableOf(this.channels);
+    }
+
+    disconnect() {
     }
 }
--- src/app/components/time-selection/time-selection.component.ts
+++ src/app/components/time-selection/time-selection.component.ts
@@ -1,5 +1,7 @@
 
-import {Component, OnInit, ChangeDetectionStrategy, AfterViewInit} from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
+import {ProjectService} from '../../project/project.service';
+import {Time, TimeInterval} from '../../time/time.model';
 
 
 @Component({
@@ -11,16 +13,28 @@ import {Component, OnInit, ChangeDetectionStrategy, AfterViewInit} from '@angula
 export class TimeSelectionComponent {
     time_start = 2000;
     time_end = 2005;
+    time: Time;
+
+    constructor(private projectService: ProjectService) {}
 
     change_start(e: Event) {
         if (e.srcElement.value > this.time_end) {
             this.time_start = this.time_end - 1;
         }
+        this.setTime();
     }
 
     change_end(e: Event) {
         if (e.srcElement.value < this.time_start) {
             this.time_end = this.time_start + 1;
         }
+        this.setTime();
+    }
+
+    setTime() {
+        let moment = require('moment')
+        this.time = new TimeInterval(moment(this.time_start + '', 'YYYY'), moment(this.time_end + '', 'YYYY'));
+        this.projectService.setTime(this.time);
+        console.log(this.time);
     }
 }
